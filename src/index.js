82941c99e11b5e5b95d1139c0dc06935264a2c1a
Vadim Buntov <vadim.buntov@elementpay.io>
2021-08-04 03:18:05 +0700

    start project

---
+++ src/index.js
@@ -0,0 +1,63 @@
+const fs = require('fs');
+const path = require('path')
+
+const graphicSymbols = {
+    long: '│',
+    mid: '├───',
+    last: '└─',
+    tab: '\t',
+}
+
+const counters = {
+    dirs: 0,
+    files: 0
+}
+
+function getBranch(lastPrefix, isLast) {
+    let prefix;
+    let branch;
+    branch = lastPrefix;
+    if (isLast) {
+       prefix = graphicSymbols.last
+       branch += graphicSymbols.tab;
+    } else {
+        prefix = graphicSymbols.mid
+        branch += (graphicSymbols.long + graphicSymbols.tab);
+    }
+    return {
+        prefix: prefix,
+        branch: branch,
+    }
+}
+
+
+async function printDirTree(dirpath, lastprefix = '', depth = 0) {
+    depth = depth - 1;
+    let dir = fs.readdirSync(dirpath, {withFileTypes: true})
+    for await (const dirent of dir) {
+        let graphics = getBranch(lastprefix, (dirent.name === dir[dir.length - 1].name))
+        if (dirent.isDirectory()) {
+            counters.dirs++;
+            console.log(lastprefix + graphics.prefix + dirent.name)
+             if ((depth > 0 || depth < 0)) await printDirTree(path.join(dirpath, dirent.name), graphics.branch, depth);
+        } else {
+            counters.files++;
+            console.log(lastprefix + graphics.prefix + dirent.name)
+        }
+    }
+}
+
+async function main() {
+    let params = getParams();
+    await printDirTree(params.path, '', params.depth);
+    Object.keys(counters).forEach((key) => { console.log(key + ' ' + counters[key]) })
+}
+
+const getParams = () => {
+    return {
+        path: process.argv[2],
+        depth: process.argv[3] === '-d' ? process.argv[4] : undefined
+    }
+}
+
+main()
